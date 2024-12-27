23fd96d3c594f69ad677075e892bf192b1de4226
dashingcoderrohan123 <anandrao23046@gmail.com>
2024-12-26 21:35:23 -0800

    Add files via upload

---
+++ lesson72.py
@@ -0,0 +1,8 @@
+
+
+shutdown = input("Do you wish to shutdown your computer ? (yes / no): ") 
+
+if shutdown == 'no': 
+	print("your computer is not shutdown") 
+else: 
+	print("Your computer is shutdown") 
